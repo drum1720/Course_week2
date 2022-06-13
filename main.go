ee4d38d4537d3543ae35d1c4139075bb2bb4f827
Shlykov_p <shlykov_p@utkonos.ru>
2022-06-13 18:17:24 +0300

    refactoring

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
 	_ "github.com/go-sql-driver/mysql"
 )
+
+var (
+	// docker run -p 3306:3306 -v $(PWD):/docker-entrypoint-initdb.d -e MYSQL_ROOT_PASSWORD=1234 -e MYSQL_DATABASE=golang -d mysql
+	DSN = "root:1234@tcp(localhost:3306)/golang?charset=utf8"
+)
+
+func main() {
+	db, err := sql.Open("mysql", DSN)
+	err = db.Ping() // вот тут будет первое подключение к базе
+	if err != nil {
+		panic(err)
+	}
+
+	handler, err := NewDbExplorer(db)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("starting server at :8082")
+	http.ListenAndServe(":8082", handler)
+}
